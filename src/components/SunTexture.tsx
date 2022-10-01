8d25264a46117da20d1c6214889a83da0bfa8bed
lucca <lucca@DESKTOP-AOTKSO1>
2022-10-01 19:16:35 +0200

    func to change the video

--- src/components/SunTexture.tsx
+++ src/components/SunTexture.tsx
@@ -1,13 +1,30 @@
+import "./SunTexture.css";
+
 interface ISunTexture {
     image: string;
     name: string;
+    link: string;
+    selectedLink: string;
+    setTexture: (link: string) => void;
+    present: (value: {
+        message: string,
+        duration: number,
+        cssClass: string,
+    },
+    ) => void;
 }
 
-function SunTexture({ image, name }: ISunTexture) {
+function SunTexture({ image, name, link, setTexture, present, selectedLink }: ISunTexture) {
     return (
-        <div>
-            <img src={image} />
-            <p>{name}</p>
+        <div className={"sunContainer"} onClick={() => {
+            setTexture(link); present({
+                message: 'Loading...',
+                duration: 3000,
+                cssClass: 'custom-loading'
+            })
+        }}>
+            <img className={"texture " + (selectedLink === link ? "selected" : "")} src={"/assets/texture/" + image} alt="image of the sun with different frequency" />
+            <p className="sunText">{name}</p>
         </div>)
 }
 export default SunTexture;
